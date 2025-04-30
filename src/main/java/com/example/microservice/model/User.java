ed95f29f843ba72f71a8de048cbe63e24a90052b
Juniër <102233050+DhuJUICE@users.noreply.github.com>
2025-04-30 22:26:36 +0200

    Update User.java

--- src/main/java/com/example/microservice/model/User.java
+++ src/main/java/com/example/microservice/model/User.java
@@ -12,13 +12,15 @@ public class User {
 
     private String username;
     private String password;
+    private String role;
 
     // Constructor, getters and setters
     public User() {}
 
-    public User(String username, String password) {
+    public User(String username, String password, String role) {
         this.username = username;
         this.password = password;
+        this.role = role;
     }
 
     public Long getId() {
@@ -43,5 +45,13 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getRole(){
+        return role;
+    }
+
+    public void setRole(String role){
+        this.role = role;
     }
 }
